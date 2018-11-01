ca755fee1902cda8ca226b45f528301d1ec91236
cosisal <cosisal@libero.it>
2018-11-01 13:46:28 +0100

    Progetti di prova
    Progetti di prova prova, swift, test

---
+++ swift/src/main/java/it/infocamere/it/swift/App.java
@@ -0,0 +1,13 @@
+package it.infocamere.it.swift;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
